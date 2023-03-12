45a40ddee5da8771c510785e44fc5b6a867e8fc2
psolis1997 <patricia-anne@outlook.com>
2023-03-13 12:30:37 +1300

    SASS mixins, interactive/styled off canvas

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 $(document).ready(function () {
 
     let url;
@@ -12,11 +14,47 @@ $(document).ready(function () {
             console.log(configData.SERVER_URL, configData.SERVER_PORT);
             url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
             console.log(url);
-            getAllProducts();
         },
         error: function (error) {
             console.log(error);
         }
     });
+
+
+    // Off Canvas Button
+    const offCanvasOpen = document.querySelector(".off-canvas-open")
+    const sidebar = document.querySelector(".sidebar")
+    const offCanvasClose = document.querySelector(".off-canvas-close")
+
+    offCanvasOpen.addEventListener("click", function () {
+        sidebar.classList.toggle("is-hidden")
+    })
+    console.log(offCanvasOpen);
+
+    offCanvasClose.addEventListener("click", function () {
+        sidebar.classList.toggle("is-hidden")
+    })
+    console.log(offCanvasClose);
+
+
+    // // Accordion
+    // const acc = document.getElementsByClassName("accordion");
+    // let i;
+
+    // for (i = 0; i < acc.length; i++) {
+    //     acc[i].addEventListener("click", function () {
+    //         /* Toggle between adding and removing the "active" class,
+    //         to highlight the button that controls the panel */
+    //         this.classList.toggle("active");
+
+    //         /* Toggle between hiding and showing the active panel */
+    //         const panel = this.nextElementSibling;
+    //         if (panel.style.display === "block") {
+    //             panel.style.display = "none";
+    //         } else {
+    //             panel.style.display = "block";
+    //         }
+    //     });
+    // }
 
 })
